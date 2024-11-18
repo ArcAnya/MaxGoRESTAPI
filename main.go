ae1914e3cc176d2944e6e6028a6b29a3788d84f9
ArcAnya <arcanyatwitch@gmail.com>
2024-11-18 20:13:22 +0100

    get single event endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gocourse.com/restapi/db"
@@ -13,9 +14,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent) // new route to get a single event by ID
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to get event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func getEvents(context *gin.Context) { // handler function for GET /events - using the gin context
