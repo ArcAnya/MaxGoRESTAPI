feec92c5b6902b94496b666dd716483595b7ff17
ArcAnya <arcanyatwitch@gmail.com>
2024-11-22 11:29:12 +0100

    add registration for events

--- routes/routes.go
+++ routes/routes.go
@@ -17,6 +17,8 @@ func RegisterRoutes(server *gin.Engine) {
 	authenticated.POST("/events", middlewares.Authenticate, createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events/:id/register", registerForEvent)
+	authenticated.DELETE("/events/:id/register", cancelRegistration)
 
 	// can also use middleware on a per route basis:
 	// server.POST("/events", middlewares.Authenticate, createEvent)
